f4727699b31a8dc0f60704f08c27fee34e2a57b8
Stephan Allen <sallen202009@gmail.com>
2018-12-28 13:16:54 -0500

    added methods to config.js file

--- webscraper-backend/config/config.js
+++ webscraper-backend/config/config.js
@@ -1,34 +1,57 @@
 const mongojs = require('mongojs');
-const db = mongojs('mongodb://sallen20:a12345@ds119323.mlab.com:19323/webscraper',['webData']);
+const db = mongojs('mongodb://sallen20:a12345@ds119323.mlab.com:19323/webscraper', ['webData']);
 
 
 const webScraperORMLite = {
-	create: (obj)=>{
-		return new Promise((resolve,reject)=>{
-			if(typeof obj === "object"){
-				db.webData.save(obj,()=>{
-					resolve('saved!')
-				});
-			}else{
-				reject('Sorry you must pass in a object!')
-			}
-		})
-	},
-	findAll: ()=>{
-		return new Promise((resolve, reject) => {
-			db.webData.find((err, docs)=>{
-				if (err) reject(err)
-				resolve(docs)
-			})
-		})
-	},
-	removeAll: ()=>{
-		return new Promise((resolve,reject)=>{
-			db.webData.remove(()=>{
-				console.log('removed!')
-			})
-		})
-	}
+  create: (obj) => {
+    return new Promise((resolve, reject) => {
+      if (typeof obj === "object") {
+        db.webData.save(obj, () => {
+          resolve('saved!')
+        });
+      } else {
+        reject('Sorry you must pass in a object!')
+      }
+    })
+  },
+  createComment: function () {
+    return new Promise((reject, resolve) => {
+      if (typeof obj === 'object') {
+        db.comment.save(obj, () => {
+          this.update(
+            /*
+            TODO: Add update function to the method
+            */
+          )
+          resolve('saved')
+        });
+      } else {
+        reject('Sorry something went wrong!')
+      }
+    })
+  },
+  findAll: () => {
+    return new Promise((resolve, reject) => {
+      db.webData.find((err, docs) => {
+        if (err) reject(err)
+        resolve(docs)
+      })
+    })
+  },
+  update: () => {
+    return new Promise((reject, resolve) => {
+      /*
+        TODO: Write update functionally
+      */
+    })
+  },
+  removeAll: () => {
+    return new Promise((resolve, reject) => {
+      db.webData.remove(() => {
+        console.log('removed!')
+      })
+    })
+  }
 }
 
 
--- webscraper-backend/schema/graphqlSchema.js
+++ webscraper-backend/schema/graphqlSchema.js
@@ -4,75 +4,75 @@ const db = require('../config/config')
 
 
 request('https://www.usatoday.com/sports/', (err, res, html) => {
-	db.removeAll()
-	if (err) throw new Error('Something went wrong')
-	const $ = cheerio.load(html)
-	$('ul.contents li').each(function (i, el) {
-		const title = $(el).text()
-		const link = $(el).children().attr("href");
-		const obj = {
-			title,
-			link
-		}
-		db.create(obj).then(res => { console.log(res) })
-	})
+  db.removeAll()
+  if (err) throw new Error('Something went wrong')
+  const $ = cheerio.load(html)
+  $('ul.contents li').each(function (i, el) {
+    const title = $(el).text()
+    const link = $(el).children().attr("href");
+    const obj = {
+      title,
+      link
+    }
+    db.create(obj).then(res => { console.log(res) })
+  })
 })
 
 const {
-	GraphQLObjectType,
-	GraphQLNonNull,
-	GraphQLSchema,
-	GraphQLString,
-	GraphQLList,
-	 } = require('graphql');
+  GraphQLObjectType,
+  GraphQLNonNull,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLList,
+} = require('graphql');
 
 const SportsNewsType = new GraphQLObjectType({
-	name: 'SportsNews',
-	fields: () => ({
-		_id: {
-			type: GraphQLString,
-			description: 'id of news data'
-		},
-		title: {
-			type: GraphQLString,
-			description: 'News article Headline'
-		},
-		link: {
-			type: GraphQLString,
-			description: 'href to the article'
-		}
-	})
+  name: 'SportsNews',
+  fields: () => ({
+    _id: {
+      type: GraphQLString,
+      description: 'id of news data'
+    },
+    title: {
+      type: GraphQLString,
+      description: 'News article Headline'
+    },
+    link: {
+      type: GraphQLString,
+      description: 'href to the article'
+    }
+  })
 })
 
 
 const rootQuery = new GraphQLObjectType({
-	name: 'RootQueryType',
-	fields: {
-		sportsnews: {
-			type: new GraphQLList(SportsNewsType),
-			resolve() {
-				return db.findAll()
-					.then(res => {
-						const arr = []
-						res.forEach(el => {
-							const obj = {
-								_id: String(el._id),
-								title: el.title,
-								link: el.link
-							}
-							arr.push(obj)
-						})
-						return (arr)
-					})
-					.catch(err => {
-						return (err)
-					})
-			}
-		}
-	}
+  name: 'RootQueryType',
+  fields: {
+    sportsnews: {
+      type: new GraphQLList(SportsNewsType),
+      resolve() {
+        return db.findAll()
+          .then(res => {
+            const arr = []
+            res.forEach(el => {
+              const obj = {
+                _id: String(el._id),
+                title: el.title,
+                link: el.link
+              }
+              arr.push(obj)
+            })
+            return (arr)
+          })
+          .catch(err => {
+            return (err)
+          })
+      }
+    }
+  }
 })
 
 
 module.exports = new GraphQLSchema({
-	query: rootQuery
+  query: rootQuery
 })
